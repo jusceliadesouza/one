6034b1538b99bc543bd8c27aac33e54bd0d0f1e4
Juscélia de Souza <jusceliadesousa@gmail.com>
2024-01-20 17:54:37 -0300

    :sparkles: feat: couse 02 challenge 04

---
+++ curso02/desafio-pratico-04/app.js
@@ -0,0 +1,18 @@
+// Desafio 01
+let listaGenerica = []
+
+// Desafio 02
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python']
+
+// Desafio 03
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang')
+
+// Desafio 04
+let nomes = ['Fernanda', 'Pedro', 'Elisa']
+console.log(nomes[0])
+
+// Desafio 05
+console.log(nomes[1])
+
+// Desafio 06
+console.log(nomes[2])
